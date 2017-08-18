3a060f4363a3c08731fae35235e910615f42c47b
Debbie Michel <debbiemichel1633@gmail.com>
2017-08-18 13:22:07 -0700

    Debug counting up numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,26 @@
 //Business logic
-var count = function(num){
-
-  if ((num) === 2){
-
-    return true;
+debugger;
+// var input = function(num){
+  var sum = 0;
+  var input = function(num){
+  for (var i = 1; i <= input; i++){
+    sum += i;
   }
-};
+}
+  console.log(input);
+  alert(input);
+  // if ((num) === 2){
+  //   return true;
+  // }else{
+  //   return false;
+  // }
 
 //user interface logic
 $(document).ready(function(){
 $("form#inPut").submit(function(event){
   event.preventDefault();
   var num = parseInt($("#inNumber").val());
-  var result = count(num);
+  var result = input(num);
 
   $("#outPut").text(result);
   //
