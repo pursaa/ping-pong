68e4462bf7ef6e2ed3a43c9e4eb7e15df179eda9
Debbie Michel <debbiemichel1633@gmail.com>
2017-08-18 13:53:33 -0700

    Debug js to get out put to deliver true or false if number is divisable by 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
 //Business logic
 debugger;
-// var input = function(num){
-  var sum = 0;
-  var input = function(num){
-  for (var i = 1; i <= input; i++){
-    sum += i;
+var input = function(num) {
+  // var result ="";
+  // for(var i = 0; i<= num; i++){
+  if (num % 3 === 0);{
+  return true;
+  } else {
+  return false;
   }
 }
-  console.log(input);
-  alert(input);
+//   var sum = 0;
+//   var input = function(num){
+//   for (var i = 1; i <= input; i++){
+//     sum += i;
+//   }
+// }
+//   console.log(input);
+//   alert(input);
   // if ((num) === 2){
   //   return true;
   // }else{
@@ -19,10 +27,10 @@ debugger;
 $(document).ready(function(){
 $("form#inPut").submit(function(event){
   event.preventDefault();
-  var num = parseInt($("#inNumber").val());
+  var num = parseInt($("input#inNumber").val());
   var result = input(num);
 
-  $("#outPut").text(result);
+  $("#outPut").text(num);
   //
   // $("#outPut").show();
 
