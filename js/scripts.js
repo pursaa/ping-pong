def5ef91baa8a9dcb70122680eb84c9d58a8d6fb
Debbie Michel <debbiemichel1633@gmail.com>
2017-08-18 16:51:38 -0700

    Clean up code indentations

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,24 @@
-var outPut = function(num) {
-  if (num % 15 === 0) {
-  return "Ping-Pong";
-  }
-  if (num % 5 === 0) {
-  return "Pong";
-  }
-  else if (num % 3 === 0) {
-  return "Ping";
-  }
-  else {
-    return num;
-  }
-};
+//business logic
+  var outPut = function(num) {
+    if (num % 15 === 0) {
+      return "Ping-Pong";
+    }if (num % 5 === 0) {
+      return "Pong";
+    }else if (num % 3 === 0) {
+      return "Ping";
+    }else {
+      return num;
+    }
+  };
 
-// jQuery user interface logic here.
-$(document).ready(function(){
-  $("#inPut").submit(function(event){
-    event.preventDefault();
-    $("#outPut").text("");
+  //user interface logic
+  $(document).ready(function(){
+    $("#inPut").submit(function(event){
+      event.preventDefault();
+      $("#outPut").text("");
 
-    var num = parseInt($("#inPutNumber").val());
-    var result = outPut(num);
-    $("#outPut").text(result);
+      var num = parseInt($("#inPutNumber").val());
+      var result = outPut(num);
+      $("#outPut").text(result);
+    });
   });
-});
