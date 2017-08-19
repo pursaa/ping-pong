9dd87a9b830d18710d7849ff0c6d6c48385d2efc
Debbie Michel <debbiemichel1633@gmail.com>
2017-08-18 20:14:17 -0700

    Game counts to input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
 //business logic
   var outPut = function(num) {
-    if (num % 15 === 0) {
-      return "Ping-Pong";
-    }if (num % 5 === 0) {
-      return "Pong";
-    }else if (num % 3 === 0) {
-      return "Ping";
-    }else {
-      return num;
-    }
-  };
 
+      var count = 0;
+      do{
+        console.log(count);
+        //  document.write(count + "<br />");
+         count++;
+      }
+
+      while (count <= num);
+
+//
+//     // if (num % 15 === 0) {
+//     //   return "Ping-Pong";
+//     // }if (num % 5 === 0) {
+//     //   return "Pong";
+//     // }else if (num % 3 === 0) {
+//     //   return "Ping";
+//     // }else {
+//     //   return num;
+//     // }
+  };
+//
   //user interface logic
   $(document).ready(function(){
     $("#inPut").submit(function(event){
