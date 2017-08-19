257c2f7c8be65d0fa8839f1b9acba8055da439c2
Debbie Michel <debbiemichel1633@gmail.com>
2017-08-18 21:45:04 -0700

    Add rules block

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,25 @@
 //business logic
   var outPut = function(num) {
 
-      var count = 0;
-      do{
-        console.log(count);
-        //  document.write(count + "<br />");
-         count++;
-      }
+      // var count = 0;
+      // do{
+      //   console.log(count);
+      //   //  document.write(count + "<br />");
+      //    count++;
+      // }
+      //
+      // while (count <= num);
 
-      while (count <= num);
-
-//
-//     // if (num % 15 === 0) {
-//     //   return "Ping-Pong";
-//     // }if (num % 5 === 0) {
-//     //   return "Pong";
-//     // }else if (num % 3 === 0) {
-//     //   return "Ping";
-//     // }else {
-//     //   return num;
-//     // }
+    if (num % 15 === 0) {
+      return "Ping-Pong";
+    }if (num % 5 === 0) {
+      return "Pong";
+    }else if (num % 3 === 0) {
+      return "Ping";
+    }else {
+      return num;
+    }
   };
-//
   //user interface logic
   $(document).ready(function(){
     $("#inPut").submit(function(event){
