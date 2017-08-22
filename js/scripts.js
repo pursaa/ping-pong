1affc3aa004435f9a11fd029244e5d497cb01d03
Debbie Michel <debbiemichel1633@gmail.com>
2017-08-21 20:19:52 -0700

    Get code back to a functioning code

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,25 @@
 //business logic
-  var outPut = function(num) {
+//business logic
+var outPut = function(num) {
+  if (num % 15 === 0) {
+    return "Ping-Pong";
+  }if (num % 5 === 0) {
+    return "Pong";
+  }else if (num % 3 === 0) {
+    return "Ping";
+  }else {
+    return num;
+  }
+};
 
-      // var count = 0;
-      // do{
-      //   console.log(count);
-      //   //  document.write(count + "<br />");
-      //    count++;
-      // }
-      //
-      // while (count <= num);
+//user interface logic
+$(document).ready(function(){
+  $("#inPut").submit(function(event){
+    event.preventDefault();
+    $("#outPut").text("");
 
-    if (num % 15 === 0) {
-      return "Ping-Pong";
-    }if (num % 5 === 0) {
-      return "Pong";
-    }else if (num % 3 === 0) {
-      return "Ping";
-    }else {
-      return num;
-    }
-  };
-  //user interface logic
-  $(document).ready(function(){
-    $("#inPut").submit(function(event){
-      event.preventDefault();
-      $("#outPut").text("");
-
-      var num = parseInt($("#inPutNumber").val());
-      var result = outPut(num);
-      $("#outPut").text(result);
-    });
+    var num = parseInt($("#inPutNumber").val());
+    var result = outPut(num);
+    $("#outPut").text(result);
   });
+});
